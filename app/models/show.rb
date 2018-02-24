8f3071584d4b40ee491e2897963e78af86de18b3
Alexander Urizar <urizaraj@gmail.com>
2018-02-24 20:45:22 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where('rating = ?', self.highest_rating).first
   end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.where('rating = ?', self.lowest_rating).first
+  end
 end
