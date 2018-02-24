d479aae57c47ed5b025d66e839c290a956e3465a
Alexander Urizar <urizaraj@gmail.com>
2018-02-24 20:41:38 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+
+  def self.most_popular_show
+    self.where('rating = ?', self.highest_rating)
+  end
 end
